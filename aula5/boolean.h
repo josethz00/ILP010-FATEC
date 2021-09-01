f8c761460792077be561f572efaaaab3f2f2226c
josethz00 <jose.soares18@fatec.sp.gov.br>
2021-08-31 21:09:57 -0300

    feat: aula5

---
+++ aula5/boolean.h
@@ -0,0 +1,7 @@
+#define false 0
+#define true 1
+#define not !
+#define and &&
+#define or ||
+
+#define bool(x) (x) ? "true" : "false"
---
+++ aula5/ex16.c
@@ -0,0 +1,11 @@
+#include "boolean.h"
+#include <stdio.h>
+
+int main() {
+  printf("%s ", bool(not false));
+  printf("%s ", bool(false and true));
+  printf("%s ", bool(true or false));
+
+  return 0;
+
+}
