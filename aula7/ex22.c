9b43a4385610031889446ee6aa1282599adc38dd
josethz00 <jose.soares18@fatec.sp.gov.br>
2021-09-22 09:19:46 -0300

    feat: aula7 ex22.c

--- aula7/ex22.c
+++ aula7/ex22.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-int numberlen(n) {
+/*int numberlen(int n) {
   int count = 0;
   do { n /= 10; ++count; } while (n != 0);
   return count;
+}*/
+
+int rev (int number){
+  int base = 1;
+
+  while (number / (base * 10)) {
+    base *= 10;
+  }
+  if (number <= 9){
+    return number;
+  } else if (number >= 10){
+    int revNum = (number % 10) * base;
+    return rev(number / 10) + revNum;
+  }
 }
 
 int main(void) {
   int a;
   printf("Digite um numero: ");
   scanf("%d", &a);
-  printf("O inverso de %d é: ", a);
-  print_reverse(a);
+  printf("O inverso de %d é: %d ", a, rev(a));
 }
